07995bc06bbe131be1b182fb079805504d4a9b84
Pip <pip.harsveld@hva.nl>
2023-06-04 15:29:47 +0200

    Make menu work

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,24 @@
-const openMenuBtn  = document.querySelector("header>div>button");
-const closeMenuBtn  = document.querySelector("header>nav>button");
+const openMenuBtn = document.querySelector("header>div>button");
+const closeMenuBtn = document.querySelector("header>nav>button");
 const nav = document.querySelector("header>nav");
+const navItems = document.querySelectorAll("header>nav>ul>li>a");
 
-openMenuBtn.addEventListener("click", function(){  
-    nav.classList.add("openMenu");
+openMenuBtn.addEventListener("click", function() {
+  nav.classList.add("openMenu");
 });
 
-closeMenuBtn.addEventListener("click", function(){  
-    nav.classList.remove("openMenu");
-    nav.classList.add("closeMenu");
+closeMenuBtn.addEventListener("click", function() {
+  closeMenu();
 });
+
+// Attach the event listener to each navItem
+navItems.forEach(function(navItem) {
+  navItem.addEventListener("click", function() {
+    closeMenu();
+  });
+});
+
+function closeMenu() {
+  nav.classList.remove("openMenu");
+  nav.classList.add("closeMenu");
+}
